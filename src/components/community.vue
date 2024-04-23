b60469826a6991fdb82bdf7b02f5922c3db9a528
mostafa12388888 <kostafa606888@gmail.com>
2024-04-23 14:08:42 +0200

    cominty

--- src/components/community.vue
+++ src/components/community.vue
@@ -1,14 +1,17 @@
 
-
-
+<script>
+</script>
 <template>
 
 
     <div>
 
-        <router-link to="/fresh-meal"><button class="circuit"></button></router-link>
-        <router-link to="/quick-delivery"><button class="circuit"></button></router-link>
-        <router-link to="/start-today"><button class="circuit"></button></router-link>
+        <router-link to="/fresh-meal"><button class="circuit"
+                :class="{ active: $route.path ==='/fresh-meal'  }"></button></router-link>
+        <router-link to="/quick-delivery"><button class="circuit"
+                :class="{ active: $route.path === '/quick-delivery' }"></button></router-link>
+        <router-link to="/start-today"><button class="circuit" :class="{ active: $route.path ==='/start-today'
+                }"></button></router-link>
     </div>
 
 </template>
@@ -20,5 +23,13 @@
     height: 40px;
     margin-right: 10px;
     cursor: pointer;
+    outline: none;
+    border: none;
+}
+.active{
+    background: #F67939;
+}
+a:hover{
+    background: none;
 }
 </style>
